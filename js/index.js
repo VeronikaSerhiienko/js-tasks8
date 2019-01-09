e2d7934dfd7713038c1fb8fc655e0ef2ac580686
Veronika Serhiienko <veronika.serhiienko1@gmail.com>
2019-01-09 21:21:58 +0200

    fix regexp, and console.log

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-var timeSearcher = /2[0-3]|[0-1]\d:[0-5]\d/;
+var timeSearcher = /2[0-3]:[0-5]\d|[0-1]*\d:[0-5]\d/;
 
 var str = 'now 09:00';
 var str1 = 'time 37:98';
 var str2 = 'breakfast at 25:99';
+var str3 = 'breakfast at 23:99';
+var str4 = 'breakfast at 9:00';
 console.log('time in string: ' + str + ' is ' + str.match(timeSearcher));
 console.log('time in string: ' + str1 + ' is ' + str1.match(timeSearcher));
-console.log('time in string: ' + str2+ ' is ' + str2.match(timeSearcher));
+console.log('time in string: ' + str2 + ' is ' + str2.match(timeSearcher));
+console.log('time in string: ' + str3 + ' is ' + str3.match(timeSearcher));
+console.log('time in string: ' + str4 + ' is ' + str4.match(timeSearcher));
 
 var arithmeticSearcher = /(-?\d+(?:\.\d+)?)\s*([-+*\/])\s*(-?\d+(?:\.\d+)?)/;
 
@@ -27,7 +31,7 @@ function createURL(templateUrl, params) {
 }
 
 var url = createURL('/api/countries/{country}/regions/{region}/', { country: 'Ukraine', region: 'Kiev' });
-
+console.log(url);
 console.log(
   url === '/api/countries/Ukraine/regions/Kiev/'
 );
